f829d51c06963e8cc92f144315eb5effa73badc7
Your Name <you@example.com>
2026-03-24 10:52:20 -0500

    Add stage to bootstrap resources to allow stages to be deployed to different regions

--- scripts/deploy-aws-resources.sh
+++ scripts/deploy-aws-resources.sh
@@ -12,10 +12,10 @@ if [ -z $GLOBAL_UNIQUE_ID ]; then
   echo "Global Unique Identifier Required";
 else
   if aws $AWS_PRF s3 sync $SSRS_SERVER_DIR/cloudformation/resources s3://$S3_DEPLOY_BUCKET_NAME/cf-templates --delete; then
-    if aws $AWS_PRF cloudformation describe-stacks --stack-name=$STACK_NAME > /dev/null 2>&1; then
-      aws $AWS_PRF cloudformation update-stack --stack-name $STACK_NAME --template-url https://$S3_DEPLOY_BUCKET_NAME.s3.$REGION.amazonaws.com/cf-templates/ssrs-template.yaml --parameters ParameterKey=Stage,ParameterValue=$STAGE ParameterKey=UniqueId,ParameterValue=$GLOBAL_UNIQUE_ID ParameterKey=TemplateLocation,ParameterValue=https://$S3_DEPLOY_BUCKET_NAME.s3.$REGION.amazonaws.com/cf-templates --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
+    if aws $AWS_PRF cloudformation describe-stacks --stack-name $STACK_NAME --region $REGION > /dev/null 2>&1; then
+      aws $AWS_PRF cloudformation update-stack --stack-name $STACK_NAME --region $REGION --template-url https://$S3_DEPLOY_BUCKET_NAME.s3.$REGION.amazonaws.com/cf-templates/ssrs-template.yaml --parameters ParameterKey=Stage,ParameterValue=$STAGE ParameterKey=UniqueId,ParameterValue=$GLOBAL_UNIQUE_ID ParameterKey=TemplateLocation,ParameterValue=https://$S3_DEPLOY_BUCKET_NAME.s3.$REGION.amazonaws.com/cf-templates --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
     else
-      aws $AWS_PRF cloudformation create-stack --stack-name $STACK_NAME --template-url https://$S3_DEPLOY_BUCKET_NAME.s3.$REGION.amazonaws.com/cf-templates/ssrs-template.yaml --parameters ParameterKey=Stage,ParameterValue=$STAGE ParameterKey=UniqueId,ParameterValue=$GLOBAL_UNIQUE_ID ParameterKey=TemplateLocation,ParameterValue=https://$S3_DEPLOY_BUCKET_NAME.s3.$REGION.amazonaws.com/cf-templates --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
+      aws $AWS_PRF cloudformation create-stack --stack-name $STACK_NAME --region $REGION --template-url https://$S3_DEPLOY_BUCKET_NAME.s3.$REGION.amazonaws.com/cf-templates/ssrs-template.yaml --parameters ParameterKey=Stage,ParameterValue=$STAGE ParameterKey=UniqueId,ParameterValue=$GLOBAL_UNIQUE_ID ParameterKey=TemplateLocation,ParameterValue=https://$S3_DEPLOY_BUCKET_NAME.s3.$REGION.amazonaws.com/cf-templates --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
     fi
   fi
 fi
